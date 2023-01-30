d8913d091c482ebb4018275c86a633a2be1c86f7
Grom679 <57178536+Grom679@users.noreply.github.com>
2023-01-30 21:44:00 +0200

    Added blink effect

--- Project_alpha/Assets/CossackGame/Scripts/Enemies/EnemyStats.cs
+++ Project_alpha/Assets/CossackGame/Scripts/Enemies/EnemyStats.cs
@@ -9,12 +9,19 @@ namespace Enemy
         #region Editor Fields
         [SerializeField]
         private EnemyScriptableObject _enemyData;
+        [SerializeField]
+        private Material _blinkMaterial;
         #endregion
 
         #region Fields
         private float _currentSpeed;
         private float _currentHealth;
         private float _currentDamage;
+
+        private float _blinkDuration = 0.125f;
+        private SpriteRenderer _renderer;
+        private Material _defaultMaterial;
+        private Coroutine _blinkRoutine;
         #endregion
 
         #region Unity Callbacks
@@ -23,6 +30,9 @@ namespace Enemy
             _currentSpeed = _enemyData.Speed;
             _currentHealth = _enemyData.MaxHealth;
             _currentDamage = _enemyData.Damage;
+
+            _renderer = GetComponent<SpriteRenderer>();
+            _defaultMaterial = _renderer.material;
         }
         #endregion
 
@@ -30,6 +40,13 @@ namespace Enemy
         public void TakeDamage(float damage)
         {
             _currentHealth -= damage;
+
+            if (_blinkRoutine != null)
+            {
+                StopCoroutine(_blinkRoutine);
+            }
+
+            _blinkRoutine = StartCoroutine(Blink());
 
             if(_currentHealth <= 0)
             {
@@ -40,6 +57,17 @@ namespace Enemy
         private void Kill()
         {
             Destroy(gameObject);
+        }
+
+        private IEnumerator Blink()
+        {
+            _renderer.material = _blinkMaterial;
+
+            yield return new WaitForSeconds(_blinkDuration);
+
+            _renderer.material = _defaultMaterial;
+
+            _blinkRoutine = null;
         }
         #endregion
     }
